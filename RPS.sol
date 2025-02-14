faf6e7f4046d84f8585e48edcef7220fb08ca9b8
Paruj Ratanaworabhan <paruj.r@gmail.com>
2025-02-15 02:31:31 +0700

    Update RPS.sol

--- RPS.sol
+++ RPS.sol
@@ -1,31 +1,35 @@
+
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
 
 contract RPS {
-    struct Player {
-        uint choice; // 0 - Rock, 1 - Paper , 2 - Scissors, 3 - undefined
-        address addr;
-    }
     uint public numPlayer = 0;
     uint public reward = 0;
-    mapping (uint => Player) public player;
+    mapping (address => uint) public player_choice; // 0 - Rock, 1 - Paper , 2 - Scissors
+    mapping(address => bool) public player_not_played;
+    address[] public players;
+
     uint public numInput = 0;
 
     function addPlayer() public payable {
         require(numPlayer < 2);
+        if (numPlayer > 0) {
+            require(msg.sender != players[0]);
+        }
         require(msg.value == 1 ether);
         reward += msg.value;
-        player[numPlayer].addr = msg.sender;
-        player[numPlayer].choice = 3;
+        player_not_played[msg.sender] = true;
+        players.push(msg.sender);
         numPlayer++;
     }
 
-    function input(uint choice, uint idx) public  {
+    function input(uint choice) public  {
         require(numPlayer == 2);
-        require(msg.sender == player[idx].addr);
+        require(player_not_played[msg.sender]);
         require(choice == 0 || choice == 1 || choice == 2);
-        player[idx].choice = choice;
+        player_choice[msg.sender] = choice;
+        player_not_played[msg.sender] = false;
         numInput++;
         if (numInput == 2) {
             _checkWinnerAndPay();
@@ -33,10 +37,10 @@ contract RPS {
     }
 
     function _checkWinnerAndPay() private {
-        uint p0Choice = player[0].choice;
-        uint p1Choice = player[1].choice;
-        address payable account0 = payable(player[0].addr);
-        address payable account1 = payable(player[1].addr);
+        uint p0Choice = player_choice[players[0]];
+        uint p1Choice = player_choice[players[1]];
+        address payable account0 = payable(players[0]);
+        address payable account1 = payable(players[1]);
         if ((p0Choice + 1) % 3 == p1Choice) {
             // to pay player[1]
             account1.transfer(reward);
